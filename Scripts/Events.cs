47f1e59bd2a6b79518b85dd9d538d721ef25b0fe
Javier <jquappev@gmail.com>
2020-07-26 00:56:19 -0400

    Add files via upload

---
+++ Scripts/Events.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Events : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Scripts/PlayerManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static bool gameOver = false;
+    public GameObject gameOverPanel;
+    public static bool isGameStarted;
+    public GameObject startingText;
+    public ParticleSystem motorIzq;
+    public ParticleSystem motorDer;
+    public GameObject tablero;
+
+    void Start()
+    {
+        gameOver = false;
+        tablero.SetActive(true);
+        Time.timeScale = 1;
+        isGameStarted = false;
+        FindObjectOfType<AudioManager>().PlaySound("Tap to start");
+        motorDer.Stop();
+        motorIzq.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver)
+        {
+            Time.timeScale = 0;
+            tablero.SetActive(false);
+            gameOverPanel.SetActive(true);
+            motorDer.Stop();
+            motorIzq.Stop();
+        }
+        if (SwipeManager.tap)
+        {
+            if (!isGameStarted)
+            {
+                Destroy(startingText);
+                FindObjectOfType<AudioManager>().StopSound("Tap to start");
+                FindObjectOfType<AudioManager>().PlaySound("Stage 1");
+                motorDer.Play();
+                motorIzq.Play();
+            }
+            isGameStarted = true;
+        }
+    }
+}
