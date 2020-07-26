88fa95401cc5c0ea8f99bc50298f5720a938b172
Javier <jquappev@gmail.com>
2020-07-26 00:56:25 -0400

    Add files via upload

---
+++ MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
